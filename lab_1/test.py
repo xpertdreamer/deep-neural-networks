083cc78046c45dee0f7d3c735f5e566c87029b3f
xpertdreamer <yungnomen@gmail.com>
2026-02-28 15:10:21 +0300

    I was testing some stuff with environment. Please, forget about this line in test.py

--- lab_1/test.py
+++ lab_1/test.py
@@ -4,6 +4,6 @@ import matplotlib
 import numpy as np
 import os
 
-print('\n\n\nIf you see this message, its OK. Congratulations!')
-print('\nCurrent environvent is ', os.environ['CONDA_DEFAULT_ENV'],'\n\n\n')
-print("Also, HI!")
+
+print("\n\n\nIf you see this message, its OK. Congratulations!")
+print("\nCurrent environvent is ", os.environ["CONDA_DEFAULT_ENV"], "\n\n\n")
