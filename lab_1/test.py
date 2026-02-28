8ed9873732e6ca0dfd1be8f75f9d4f6e6d4e2620
xpertdreamer <yungnomen@gmail.com>
2026-02-28 15:08:42 +0300

    Initialized new repo && First lab requirements

---
+++ lab_1/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib
+import numpy as np
+import os
+
+print('\n\n\nIf you see this message, its OK. Congratulations!')
+print('\nCurrent environvent is ', os.environ['CONDA_DEFAULT_ENV'],'\n\n\n')
+print("Also, HI!")
